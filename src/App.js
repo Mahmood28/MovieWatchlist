5a0e745315b230c7f397df85f769ddfd58d55b19
AymanAlshaikh <ayman159@live.com>
2021-02-02 16:04:40 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
 import movies from "./movies";
-=======
+
 import WatchList from "./components/WatchList";
->>>>>>> 783560861a3d57c6e484fbf35c042fd281dbd7d2
 
 function App() {
   return (
